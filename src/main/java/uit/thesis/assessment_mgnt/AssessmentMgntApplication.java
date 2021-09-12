f3f147f9ee8f5ae6a130f38083ee5175062f8189
thienanh <todiepthienanh@gmail.com>
2021-09-12 14:17:39 +0700

    Create Project

---
+++ src/main/java/uit/thesis/assessment_mgnt/AssessmentMgntApplication.java
@@ -0,0 +1,13 @@
+package uit.thesis.assessment_mgnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentMgntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentMgntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uit/thesis/assessment_mgnt/AssessmentMgntApplicationTests.java
@@ -0,0 +1,13 @@
+package uit.thesis.assessment_mgnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentMgntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
